f36e915597d40af06d1f8a113ce7ea6507a33185
Ivan Vasilev <tor419@yandex.ru>
2023-04-08 20:15:56 +0700

    chore: three commit

--- src/test/java/ru/netology/CardAutoTest.java
+++ src/test/java/ru/netology/CardAutoTest.java
@@ -1,19 +1,14 @@
 package ru.netology;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import javax.swing.*;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CardAutoTest {
     private WebDriver driver;
@@ -43,10 +38,10 @@ public class CardAutoTest {
         List<WebElement> inputs = driver.findElements(By.tagName("input"));
         inputs.get(0).sendKeys("Сидоров Иван");
         inputs.get(1).sendKeys("+79856239080");
-        driver.findElement(By.className("Checkbox")).click();
+        driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.className("paragraph paragraph_theme_alfa-on-white")).getText().trim();
+        String actual = driver.findElement(By.className("order-success")).getText().trim();
         assertEquals(expected, actual);
     }
 }
